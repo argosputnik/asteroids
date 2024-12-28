776847527e3bca1443d1868aa7c06eaab848988b
argosputnik <schmidtjai@yandex.com>
2024-12-28 20:25:38 +0400

    Just beginning part 3

---
+++ asteroid.py
@@ -0,0 +1,10 @@
+import pygame
+from CircleShape import *
+
+
+class Asteroid(CircleShape)
+    def __init__(self, x, y, radius):
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.circle(), 2)
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,6 +14,11 @@ def main():
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     run = True
+    updateable =  pygame.sprite.Group()
+    drawable   =  pygame.sprite.Group()
+    updateable.add(player)
+    drawable.add(player)
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,13 +26,18 @@ def main():
 
 
         screen.fill((0, 0, 0))  
-
         
         clock.tick(60)
         dt = clock.get_time() / 1000
 
+        for players in updateable:
+            players.update(dt)        
+
+
+        for players in drawable:
+            players.draw(screen)        
+
         
-        player.draw(screen)
 
         
         pygame.display.flip()
